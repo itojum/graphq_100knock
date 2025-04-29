cf985ed3f12822abf741d87f4462ade90df679a4
itojum <itojum1230@gmail.com>
2025-04-29 17:32:59 +0900

    productIdとuserIdをUUID形式に更新

--- client/41_50/41/index.ts
+++ client/41_50/41/index.ts
@@ -6,8 +6,8 @@ const query = await Deno.readTextFile(new URL('./query.graphql', import.meta.url
 const variables = {
   content: "バッテリーの持ちが良く、日常使いに最適です。",
   rating: 5,
-  productId: "1",
-  userId: "1",
+  productId: "d13c6a61-53f5-43b9-97d0-15b8913e8e3d",
+  userId: "a7b8c9d0-e1f2-a3b4-c5d6-e7f8a9b0c1d2",
 }
 
 request('http://127.0.0.1:4000/', query, variables)
